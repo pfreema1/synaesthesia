2c90962f1c9708a3131382909821a354b19dfcab
Paul <pfreema1@gmail.com>
2018-04-07 17:09:23 -0400

    stationary boxes

--- src/modules/App/Scene.js
+++ src/modules/App/Scene.js
@@ -10,53 +10,81 @@ const CANVAS_HEIGHT = Math.max(
   window.innerHeight || 0
 );
 
+const COLOR_PALETTE = [
+  "RGBA(7, 28, 55, 1.00)",
+  "RGBA(78, 63, 118, 1.00)",
+  "RGBA(61, 99, 149, 1.00)",
+  "RGBA(173, 216, 223, 1.00)",
+  "RGBA(214, 95, 108, 1.00)",
+  "RGBA(143, 60, 76, 1.00)"
+];
+
+const randomColorFromPalette = () => {
+  return COLOR_PALETTE[parseInt(Math.random() * 5, 10)];
+};
+
 class Scene extends Component {
   componentDidMount() {
+    // const stats;
+    // this.camera, scene, raycaster, renderer;
+
+    // const mouse = new THREE.Vector2(), INTERSECTED;
+    this.radius = 500;
+    this.theta = 0;
+    this.frustumSize = 1000;
+
     const width = CANVAS_WIDTH;
     const height = CANVAS_HEIGHT;
 
     //renderer
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setClearColor("#000000");
-    renderer.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setClearColor("blue");
+    this.renderer.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
 
     //camera
-    const camera = new THREE.PerspectiveCamera(35, width / height, 0.1, 3000);
-    camera.position.z = 4;
+    this.aspect = window.innerWidth / window.innerHeight;
+    this.camera = new THREE.OrthographicCamera(
+      this.frustumSize * this.aspect / -2,
+      this.frustumSize * this.aspect / 2,
+      this.frustumSize / 2,
+      this.frustumSize / -2,
+      1,
+      1000
+    );
+    this.camera.position.z = 0;
 
     //scene
-    const scene = new THREE.Scene();
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xf0f0f0);
 
     //light
-    const light = new THREE.AmbientLight(0xffffff, 0.5);
-    scene.add(light);
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(1, 1, 1).normalize();
+    this.scene.add(light);
 
-    const light1 = new THREE.PointLight(0xffffff, 0.5);
-    scene.add(light1);
+    const geometry = new THREE.BoxBufferGeometry(20, 20, 20);
 
-    const geometry = new THREE.BoxGeometry(100, 100, 100);
-    const material = new THREE.MeshLambertMaterial({ color: "yellow" });
-    const cube1 = new THREE.Mesh(geometry, material);
-    cube1.position.set(0, 0, -1000);
-    scene.add(cube1);
+    for (let i = 0; i < 2000; i++) {
+      let object = new THREE.Mesh(
+        geometry,
+        new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff })
+      );
 
-    const cube2 = new THREE.Mesh(geometry, material);
-    cube2.position.set(100, 200, -1000);
-    scene.add(cube2);
+      object.position.x = Math.random() * 800 - 400;
+      object.position.y = Math.random() * 800 - 400;
+      object.position.z = Math.random() * 800 - 400;
 
-    //static cube
-    const cube3 = new THREE.Mesh(geometry, material);
-    cube3.position.set(-150, -100, -1000);
-    cube3.matrixAutoUpdate = false;
-    cube3.updateMatrix();
-    scene.add(cube3);
+      object.rotation.x = Math.random() * 2 * Math.PI;
+      object.rotation.y = Math.random() * 2 * Math.PI;
+      object.rotation.z = Math.random() * 2 * Math.PI;
 
-    this.scene = scene;
-    this.camera = camera;
-    this.renderer = renderer;
-    this.material = material;
-    this.cube1 = cube1;
-    this.cube2 = cube2;
+      object.scale.x = Math.random() + 0.5;
+      object.scale.y = Math.random() + 0.5;
+      object.scale.z = Math.random() + 0.5;
+
+      this.scene.add(object);
+    }
 
     this.mount.appendChild(this.renderer.domElement);
     this.start();
@@ -79,11 +107,7 @@ class Scene extends Component {
 
   // this function is called over and over
   animate = () => {
-    this.cube1.rotation.x += 0.001;
-    this.cube1.rotation.y += 0.0005;
-
-    this.cube2.rotation.x += 0.002;
-    this.cube2.rotation.y += 0.0003;
+    this.theta += 0.1;
 
     this.renderScene();
     this.frameId = window.requestAnimationFrame(this.animate);
